361ae14da2bf3c4e0c8308cf2352ccad6eb04a6f
Jason Weirather <jason-weirather@users.noreply.github.com>
2019-02-17 01:16:57 -0500

    working for generating examples

--- multiplexifsimulator/__init__.py
+++ multiplexifsimulator/__init__.py
@@ -1,6 +1,7 @@
 from pythologist.formats.utilities import watershed_image, map_image_ids, image_edges
 import numpy as np
-import sys
+import pandas as pd
+import sys, random
 class FrameEmitter(object):
     def __init__(self,*args,**kwargs):
         self.shape = kwargs['shape']
@@ -9,6 +10,7 @@ class FrameEmitter(object):
         return
     def set_cell_coordinates(self,cell_df):
         # cell_df: <x> <y> <id (optional)>
+        # if an id is given then that will be used.  otherwise we shuffle
         if cell_df['x'].max() >= self.shape[1]:
             sys.stderr.write("Warning: shape is larger than coordinates... trimming")
             cell_df = cell_df.loc[cell_df['x']<self.shape[1]]
@@ -30,11 +32,58 @@ class FrameEmitter(object):
             nuc[row['y']][row['x']] = row.name
         start = map_image_ids(nuc,remove_zero=False).query('id!=0').apply(lambda x: (x['x'],x['y']),1)
         finish = map_image_ids(nuc,remove_zero=False).query('id==0').apply(lambda x: (x['x'],x['y']),1)
-        nuc1 = watershed_image(nuc,list(start),list(finish),steps=self.cell_steps)
+        nuc1 = watershed_image(nuc,list(start),list(finish),steps=self.cell_steps,border=0)
         self.cell_image = nuc1
         self.edge_image = image_edges(nuc1,seek_distance=1)
         start = map_image_ids(nuc1,remove_zero=False).query('id!=0').apply(lambda x: (x['x'],x['y']),1)
         finish = map_image_ids(nuc1,remove_zero=False).query('id==0').apply(lambda x: (x['x'],x['y']),1)
-        temp = watershed_image(nuc1,list(start),list(finish),steps=self.boundary_steps)
+        temp = watershed_image(nuc1,list(start),list(finish),steps=self.boundary_steps,border=0)
         self.processed_image = temp.astype(np.bool).astype(np.uint8)
         return self.cell_image, self.edge_image, self.processed_image
+    def gradient_model1(self):
+        xdim = self.shape[1]
+        ydim = self.shape[0]
+        cells = []
+        def _make_gradient1(x,xdim,label):
+            # we want 1/3 to 2/3 drop off of tumor
+            fill = 'TUMOR'
+            if x < xdim/3: return fill
+            if x > 2*xdim/3: return label
+            if (x-(xdim/3)) > random.random()*xdim/3: return label
+            return fill
+        def _make_gradient2(x,xdim,label):
+            # we want a 1 to 0 gradient of t-cell
+            fill = 'T-CELL'
+            if (1-x/xdim) < random.random() and random.random() < 0.2: return fill
+            return label
+        def _make_gradient3(x,y,xdim,ydim,label):
+            # we want a 1 to 0 gradient of t-cell
+            fill = '+'
+            if x/xdim < random.random() and label == 'T-CELL' and y/ydim > 0.5: return '+'
+            return '-'
+
+        for y in range(5,ydim,10):
+            for x in range(5,xdim,10):
+                cells.append([x,y])
+        cells = pd.DataFrame(cells,columns=['x','y']).reset_index().\
+            rename(columns={'index':'id'})
+        ## Lets start everything as OTHER
+        cells['phenotype_label'] = 'OTHER'
+        ## Lets start PD1 as negative on everything
+        cells['PD1'] = '-'
+        ## Lets start PDL1 as negative on everything
+        cells['PDL1'] = '-'
+        cells['phenotype_label'] = cells.apply(lambda x:
+            _make_gradient1(x['x'],xdim,x['phenotype_label'])
+        ,1)
+        cells['phenotype_label'] = cells.apply(lambda x:
+            _make_gradient2(x['x'],xdim,x['phenotype_label'])
+        ,1)
+        # Make a hard PDL1 gradient on the top
+        cells['PDL1'] = cells.apply(lambda x: '+' if x['y'] > ydim/2 and x['phenotype_label']=='TUMOR'  and random.random() < 0.9 else '-',1)
+        cells['PD1'] = cells.apply(lambda x: 
+            _make_gradient3(x['x'],x['y'],xdim,ydim,x['phenotype_label'])
+        ,1)
+        cells = cells.sort_values('phenotype_label').set_index('id').reset_index(drop=True).reset_index().\
+            rename(columns={'index':'id'})
+        return cells
--- multiplexifsimulator/formats/inform.py
+++ multiplexifsimulator/formats/inform.py
@@ -1,6 +1,8 @@
 from multiplexifsimulator import FrameEmitter
 from tifffile import TiffWriter
 import numpy as np
+import pandas as pd
+import os
 _nuc_xml = '<?xml version="1.0" encoding="utf-16"?>\r\n<SegmentationImage>\r\n <Version>1</Version>\r\n <CompartmentType>Nucleus</CompartmentType>\r\n</SegmentationImage>'
 _mem_xml = '<?xml version="1.0" encoding="utf-16"?>\r\n<SegmentationImage>\r\n <Version>1</Version>\r\n <CompartmentType>Membrane</CompartmentType>\r\n</SegmentationImage>'
 _pro_xml = '<?xml version="1.0" encoding="utf-16"?>\r\n<ProcessRegionImage>\r\n <Version>1</Version>\r\n</ProcessRegionImage>'
@@ -13,9 +15,82 @@ class FrameEmitterInForm(FrameEmitter):
     def save_binary_seg_maps(self,path,processed_image=True):
         with TiffWriter(path) as tif:
             tif.save(self.cell_image.astype(np.uint16), 
-                     compress=9, extratags=[(270,'s',1,_nuc_xml,True)])
+                     compress=9, extratags=[('ImageDescription','s',0,_nuc_xml,True)])
             tif.save(self.edge_image.astype(np.uint16), 
-                     compress=9, extratags=[(270,'s',1,_mem_xml,True)])
+                     compress=9, extratags=[('ImageDescription','s',0,_mem_xml,True)])
             if processed_image:
                 tif.save(self.processed_image.astype(np.uint16), 
-                         compress=9, extratags=[(270,'s',1,_pro_xml,True)])
+                         compress=9, extratags=[('ImageDescription','s',0,_pro_xml,True)])
+    def save_binary_seg_maps_r(self,path,processed_image=True):
+        with TiffWriter(path) as tif:
+            tif.save(self.cell_image.astype(np.uint16), 
+                     compress=9, description=_nuc_xml)
+            tif.save(self.edge_image.astype(np.uint16), 
+                     compress=9, description=_mem_xml)
+            if processed_image:
+                tif.save(self.processed_image.astype(np.uint16), 
+                         compress=9, description=_pro_xml)
+
+    def make_inform_frame(self,base_path,sample_name,frame_name):
+        cells = self.gradient_model1()
+        self.set_cell_coordinates(cells)
+        cell_seg = _construct_cell_seg(cells)
+        score = _construct_score(cells)
+        score.loc[:,'Sample Name'] = frame_name
+        path = os.path.join(base_path,sample_name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        cell_seg.to_csv(os.path.join(path,sample_name+'_'+frame_name+'_cell_seg_data.txt'),
+            index=False,sep="\t")
+        score.to_csv(os.path.join(path,sample_name+'_'+frame_name+'_score_data.txt'),
+            index=False,sep="\t")
+        self.make_cell_image()
+        bfile = os.path.join(path,sample_name+'_'+frame_name+'_binary_seg_maps.tif')
+        self.save_binary_seg_maps(bfile,processed_image=True)
+        return #path,cell_seg,score
+        
+
+def _construct_cell_seg(cells):
+    header = ['Cell ID','Cell X Position','Cell Y Position','Nucleus Area (pixels)',
+          'Nucleus Area (percent)','Nucleus Compactness','Nucleus Minor Axis',
+          'Nucleus Major Axis','Entire Cell PD-1 (Opal 540) Mean (Normalized Counts, Total Weighting)',
+          'Nucleus PD-1 (Opal 540) Mean (Normalized Counts, Total Weighting)',
+          'Membrane PD-1 (Opal 540) Mean (Normalized Counts, Total Weighting)',
+          'Membrane Area (pixels)','Membrane Area (percent)','Membrane Compactness',
+          'Membrane Minor Axis','Membrane Major Axis',
+          'Nucleus PD-Ligand-1 (Opal 690) Mean (Normalized Counts, Total Weighting)',
+          'Entire Cell PD-Ligand-1 (Opal 690) Mean (Normalized Counts, Total Weighting)',
+          'Membrane PD-Ligand-1 (Opal 690) Mean (Normalized Counts, Total Weighting)',
+          'Entire Cell Area (pixels)',
+          'Entire Cell Area (percent)','Entire Cell Compactness',
+          'Entire Cell Minor Axis','Entire Cell Major Axis','Phenotype','Confidence']
+    cs = cells.copy().rename(columns={'id':'Cell ID',
+                                  'x':'Cell X Position',
+                                  'y':'Cell Y Position',
+                                  'phenotype_label':'Phenotype'})
+    fill_in = [x for x in header if x not in cs.columns]
+    for col in fill_in: cs[col] = 1
+    cs.loc[cs['PD1']=='-',cs.columns.str.contains('PD-1')] = 0
+    cs.loc[cs['PDL1']=='-',cs.columns.str.contains('PD-Ligand-1')] = 0
+    return cs.drop(columns=['PD1','PDL1'])
+
+def _construct_score(cells):
+    header = ['Path','Sample Name','Tissue Category',
+          'First Cell Compartment','First Stain Component',
+          'Second Cell Compartment','Second Stain Component',
+          'Double Negative','Single PD-Ligand-1 (Opal 690)',
+          'Single PD-1 (Opal 540)','Double Positive',
+          'Tissue Category Area (Percent)','Number of Cells',
+          'PD-Ligand-1 (Opal 690) Threshold',
+          'PD-1 (Opal 540) Threshold','Lab ID','Slide ID',
+          'TMA Sector','TMA Row','TMA Column','TMA Field',
+          'inForm 2.1.5430.24864']
+    score = pd.DataFrame([len(header)*[0.5]],columns=header)
+    score['Path'] = '/location'
+    score['Sample Name'] = 'sample_name'
+    score['Tissue Category'] = ''
+    score['First Stain Component'] = 'PD-Ligand-1 (Opal 690)'
+    score['Second Stain Component'] = 'PD-1 (Opal 540)'
+    score['First Cell Compartment'] = 'Membrane'
+    score['Second Cell Compartment'] = 'Nucleus'
+    return score
